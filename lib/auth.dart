8986db2c8f6446b7aae0a633708fd8fc53e41f15
ThetaLog(n) <giorgiomartucci02@gmail.com>
2024-07-12 15:53:44 +0200

    Commit iniziale

---
+++ lib/auth.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Auth{
+  final FirebaseAuth _firebaseAuth=FirebaseAuth.instance;
+  User? get currentUser => _firebaseAuth.currentUser();
+  Stream<User?> get authStateChanges => _firebaseAuth.onAuthStateChanged;
+
+  Future<void> signInWithEmailAndPassword() async{
+
+  }
+
+
+}
